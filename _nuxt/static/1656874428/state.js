c54f728b73941beafc74fd7f05399858961e458c
thejoeejoee <thejoeejoee@users.noreply.github.com>
2022-07-04 06:47:27 +0000

    deploy: d52eb371f6d1ddb72057e3b1f084003cd3928e89

--- _nuxt/static/1656874428/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fqango-game\u002F_nuxt\u002Fstatic\u002F1656874428",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fqango-game\u002F",assetsPath:"\u002Fqango-game\u002F_nuxt\u002F",cdnURL:a}}}}(null));
